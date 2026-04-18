ed0e06cb04ebdfa873c97067628f1b419a9cd38f
Bohdan Kryzhov <kb251_kbs@student.ztu.edu.ua>
2026-04-18 16:47:23 +0200

    Add Logger + application startup/shutdown logging

--- OOPWPFProject/App.xaml.cs
+++ OOPWPFProject/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using System.IO;
+
+using OOPWPFProject.Models;
+using System.Globalization;
 
 namespace OOPWPFProject
 {
@@ -7,6 +11,33 @@ namespace OOPWPFProject
     /// </summary>
     public partial class App : Application
     {
-    }
+        public static string DataDirectoryPath { get; private set; } = string.Empty;
+        public static string LogFilePath => Path.Combine( App.DataDirectoryPath, "Log.txt" );
+        public static string SaveFilePath => Path.Combine( App.DataDirectoryPath, "Save.json" );
 
+        static DateTime  StartTime { get; } = DateTime.Now;
+
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            base.OnStartup( e );
+
+            DataDirectoryPath = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.MyDocuments ), "Data");
+
+            if ( !Directory.Exists( DataDirectoryPath ) )
+            {
+                Directory.CreateDirectory( DataDirectoryPath );
+            }
+
+            Logger.LogInfo( "Програма почала роботу");
+        }
+
+        protected override void OnExit( ExitEventArgs e )
+        {
+            base.OnExit( e );
+
+            var EndTime = DateTime.Now;
+            var workingTime = (EndTime - StartTime).ToString(@"hh\:mm\:ss", CultureInfo.InvariantCulture);
+            Logger.LogInfo( $"Програма завершила роботу (Час роботи  {workingTime} )" );
+        }
+    }
 }
---
+++ OOPWPFProject/Models/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace OOPWPFProject.Models;
+
+public static class Logger
+{
+    public static void LogInfo(string message)
+    {
+        try
+        {
+            string logEntry = $"[{DateTime.Now:HH:mm:ss dd/MM}] -> {message}{Environment.NewLine}";
+
+            File.AppendAllText(App.LogFilePath, logEntry);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Помилка запису: {ex.Message}");
+        }
+    }
+}
