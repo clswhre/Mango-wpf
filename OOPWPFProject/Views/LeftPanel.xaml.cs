ff5d1978d0c048ff8e4eeb13d3d0e1797b968f01
Bohdan Kryzhov <kb251_kbs@student.ztu.edu.ua>
2026-05-18 16:18:01 +0300

    splited ui into left panel and maincontent for future

---
+++ OOPWPFProject/Views/LeftPanel.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace OOPWPFProject.Views;
+
+public partial class LeftPanel : UserControl
+{
+    public LeftPanel()
+    {
+        InitializeComponent();
+    }
+}
